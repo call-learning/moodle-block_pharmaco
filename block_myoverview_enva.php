215e196a025cb5cf02aa6f05f27e2fe91612a23b
Laurent David <lmedavid@gmail.com>
2018-06-28 15:55:02 +0200

    ENVA: Add myoverview derivated block and scheduled tasks to make sure users keeps on being registered to the test course

---
+++ block_myoverview_enva.php
@@ -0,0 +1,121 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block myoverview_enva is defined here.
+ *
+ * @package     block_myoverview_enva
+ * @copyright   2018 Laurent David <laurent@call-learning.fr>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+global $CFG;
+require_once($CFG->dirroot.'/blocks/myoverview/block_myoverview.php');
+require_once($CFG->libdir . '/completionlib.php');
+/**
+ * myoverview_enva block.
+ *
+ * @package    block_myoverview_enva
+ * @copyright  2018 Laurent David <laurent@call-learning.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_myoverview_enva extends block_myoverview {
+    /**
+     * Default threshold to display a course a to be reviewed or not
+     */
+    public static $DEFAULT_REVIEW_THRESHOLD = 0.5;
+    
+    /**
+     * Initializes class member variables.
+     */
+    public function init() {
+        // Needed by Moodle to differentiate between blocks.
+        $this->title = get_string('pluginname', 'block_myoverview_enva');
+    }
+
+    /**
+     * Returns the block contents.
+     *
+     * @return stdClass The block contents.
+     */
+    public function get_content() {
+        global $USER, $CFG, $DB;
+/*        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        if (empty($this->instance)) {
+            $this->content = '';
+            return $this->content;
+        }
+  */
+        $is_external = \local_enva\helper::is_user_external_role($USER->id);
+        if (!$is_external) {
+            return parent::get_content();
+        }
+        $this->content = new stdClass();
+        $this->content->text = '';
+        $this->content->footer = '';
+    
+        // Here we either display the link to the test course or the list of course ordered by their scores
+        
+        // Check first if we have completed the test course
+        $courseid = \local_enva\helper::get_test_course_id();
+        $course = $DB->get_record('course', array('id'=>$courseid));
+        
+        $ccompletion = new completion_completion(array('course' => $courseid, 'userid' => $USER->id));
+        $renderer = $this->page->get_renderer('block_myoverview_enva');
+        
+        
+        if (!$ccompletion->is_complete()) {
+            $tcp = new test_course_prompt($USER->id, $course);
+            $this->content->text =  $renderer->render($tcp);
+        } else {
+            $reviewthreshold = block_myoverview_enva::$DEFAULT_REVIEW_THRESHOLD;
+            if (isset($this->config->review_threshold)) {
+                $reviewthreshold = $this->config->review_threshold;
+            }
+            $ocl = new ordered_course_list($USER->id, $reviewthreshold);
+            $this->content->text =  $renderer->render($ocl);
+        }
+        
+        return $this->content;
+    }
+
+    /**
+     * Defines configuration data.
+     *
+     * The function is called immediatly after init().
+     */
+    public function specialization() {
+
+        // Load user defined title and make sure it's never empty.
+        if (empty($this->config->title)) {
+            $this->title = get_string('pluginname', 'block_myoverview');
+            // We show the parent's title
+        } else {
+            $this->title = $this->config->title;
+        }
+    }
+
+    /**
+     * Enables global configuration of the block in settings.php.
+     *
+     * @return bool True if the global configuration is enabled.
+     */
+    function has_config() {
+        return true;
+    }
+}
---
+++ renderer.php
@@ -0,0 +1,125 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_myoverview_enva_renderer extends plugin_renderer_base {
+
+    public function render_test_course_prompt(test_course_prompt $tp) {
+        return $this->render_from_template('block_myoverview_enva/complete_course', $tp->export_for_template($this));
+    }
+    public function render_ordered_course_list(ordered_course_list $cl) {
+        return $this->render_from_template('block_myoverview_enva/course-list', $cl->export_for_template($this));
+    }
+}
+
+class test_course_prompt implements  renderable, templatable {
+    public $userid;
+    public $course;
+    
+    /**
+     * Constructor.
+     *
+     * @param string $tab The tab to display.
+     */
+    public function __construct($userid, $course) {
+        $this->userid  = $userid;
+        $this->course  = $course;
+    }
+    
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param \renderer_base $output
+     * @return stdClass
+     */
+    public function export_for_template(renderer_base $output) {
+        global $CFG;
+        
+        $percentage = \core_completion\progress::get_course_progress_percentage($this->course, $this->userid);
+        if (!is_null($percentage)) {
+            $percentage = floor($percentage);
+        }
+    
+        if (!is_null($percentage)) {
+            $percentage = floor($percentage);
+        }
+        $courselink = new action_link(
+            new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $this->course->id)),
+            ($percentage == 0)? get_string('launchcourse:first','block_myoverview_enva'): get_string('launchcourse:started','block_myoverview_enva'));
+    
+        $contextdata = [
+            'fullname' => $this->course->fullname,
+            'progress'=> $percentage,
+            'viewurl' => new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $this->course->id)),
+            'hasstarted'=> $percentage >0
+        ];
+        $contextdata += (array)$courselink->export_for_template($output);
+        return $contextdata;
+    }
+}
+
+
+class ordered_course_list implements  renderable, templatable {
+    public $userid;
+    public $reviewthreshold;
+    /**
+     * Constructor.
+     *
+     * @param string $tab The tab to display.
+     */
+    public function __construct($userid, $reviewthreshold) {
+        $this->userid  = $userid;
+        $this->reviewthreshold  = $reviewthreshold;
+    }
+    
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param \renderer_base $output
+     * @return stdClass
+     */
+    public function export_for_template(renderer_base $output) {
+        global $CFG;
+    
+        $ts = new \local_enva\tag_scores(\local_enva\helper::get_test_course_id(), $this->userid);
+        $cl = new \local_enva\course_list_by_score($this->userid, $ts->compute());
+        $courselist = $cl->get_list();
+        
+        // Make sure we add the necessary information for the template in the list
+        $courselistarray = [];
+        
+        $selectioncourseid = \local_enva\helper::get_test_course_id(); // We remove the test course from the list
+        // and add it at the end
+        $selectioncourse = null;
+        foreach ($courselist as $c)  {
+            if ($c->id != $selectioncourseid) {
+                $c->viewurl = new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $c->id));
+                $c->hasstarted = $c->progress > 0;
+                $c->tobreviewed = ($c->score < $this->reviewthreshold);
+                $courselistarray[] = (array)$c;
+            } else {
+                $c->viewurl = new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $c->id));
+                $c->hasstarted = true ;
+                $c->tobreviewed = false;
+                $c->visible = false;
+                $selectioncourse = $c;
+            }
+        }
+        $courselistarray[] = $selectioncourse; // Add test course at the end
+        return array( 'courses' => $courselistarray );
+    }
+}
